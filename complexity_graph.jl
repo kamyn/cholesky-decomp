2fc616996513522ade686855e849687b40aa83b0
pavel <kamynin.pavel.74@gmail.com>
2022-12-15 02:52:58 +0300

    nrate added

--- complexity_graph.jl
+++ complexity_graph.jl
@@ -11,10 +11,20 @@ for idx in 1:lastindex(ns)
     local A = 100*rand(n, n)
     A = A'*A # symmetric positive definite matrix
     BenchmarkTools.DEFAULT_PARAMETERS.seconds = n/10
-    t = @benchmark CholeskyDecomp.decomposition($A)
+    local t = @benchmark CholeskyDecomp.decomposition($A)
     medians[idx] = time(median(t)) / 1000000000
 end
+
+nrate = 0.
+for i in 1:lastindex(ns)-1
+    for j in i+1:lastindex(ns)
+        global nrate += medians[j] / medians[i] * 2^3/(ns[j]/ns[i])^3
+    end
+end
+nrate = 2*nrate / ( (lastindex(ns)-1)*lastindex(ns) ) 
 
 plot(ns, medians, seriestype=:scatter, label=false)
 xlabel!("n")
 ylabel!("time, seconds")
+
+println("Increase time from 2n input data: ", nrate)
